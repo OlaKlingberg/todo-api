b11f8cfc3bbb71f8eb2b0390babb14cad075f65c
Ola Klingberg <ola@olaklingberg.com>
2016-10-17 12:21:16 -0400

    Adds third todo-item.

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
 var express = require('express');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+        id: 1,
+        description: "Meet mom for lunch",
+        completed: false
+    },
+    {
+        id: 2,
+        description: "Go to market",
+        completed: false
+    },
+    {
+        id: 3,
+        description: "Knit a doily",
+        completed: false
+    }];
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.send('Todo API Root');
 });
 
-app.listen(PORT, function() {
+app.get('/todos', function(req, res) {
+    res.json(todos);
+});
+
+app.get('todos/:id', function(req, res) {
+
+})
+
+app.listen(PORT, function () {
     console.log("Express listening on port " + PORT + "!");
 });
 

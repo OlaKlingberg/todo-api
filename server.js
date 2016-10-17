8b3b0621f539ddf65b859bcf69ea47a4583a1a49
Ola Klingberg <ola@olaklingberg.com>
2016-10-17 16:31:33 -0400

    Refactors API with underscore.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -12,21 +13,15 @@ app.get('/', function (req, res) {
     res.send('Todo API Root');
 });
 
-app.get('/todos', function(req, res) {
+app.get('/todos', function (req, res) {
     res.json(todos);
 });
 
-app.get('/todos/:id', function(req, res) {
+app.get('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo;
+    var matchedTodo = _.findWhere(todos, {id: todoId});
 
-    todos.forEach(function(todo) {
-        if (todoId === todo.id) {
-            matchedTodo = todo;
-        }
-    });
-
-    if (matchedTodo) {
+    if ( matchedTodo ) {
         res.json(matchedTodo);
     } else {
         res.status(404).send();
@@ -35,16 +30,20 @@ app.get('/todos/:id', function(req, res) {
 
 });
 
-app.post('/todos', function(req, res) {
-    var body = req.body;
+app.post('/todos', function (req, res) {
+    var body = _.pick(req.body, 'description', 'completed');
 
+    if ( !_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ) {
+        return res.status(400).send();
+    }
+
+    body.description = body.description.trim();
     body.id = todoNextId++;
+
     todos.push(body);
 
     res.json(body);
 });
-
-
 
 
 app.listen(PORT, function () {

686b188720e6654330d7384795752fc6a87b8da7
Ola Klingberg <ola@olaklingberg.com>
2016-10-17 22:22:31 -0400

    Adds PUT /todos/:id

--- server.js
+++ server.js
@@ -45,7 +45,6 @@ app.post('/todos', function (req, res) {
     res.json(body);
 });
 
-// DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
     var matchedTodo = _.findWhere(todos, {id: todoId});
@@ -56,6 +55,32 @@ app.delete('/todos/:id', function(req, res) {
         todos = _.without(todos, matchedTodo);
         res.json(matchedTodo);
     }
+});
+
+
+app.put('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+
+    if (!matchedTodo) {
+        return res.status(404).send();
+    }
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+    _.extend(matchedTodo, validAttributes);
+    res.json(matchedTodo);
+
 
 
 
@@ -65,14 +90,4 @@ app.delete('/todos/:id', function(req, res) {
 app.listen(PORT, function () {
     console.log("Express listening on port " + PORT + "!");
 });
-
-
-
-
-
-
-
-
-
-
 

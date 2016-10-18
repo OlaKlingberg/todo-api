650531a0f123f5950aff1371343248d5a2e57ecd
Ola Klingberg <ola@olaklingberg.com>
2016-10-18 08:02:26 -0400

    Adds completed filter.

---
+++ variable-playground.js
@@ -0,0 +1,29 @@
+// var person = {
+//     name: 'Andrew',
+//     age: 21
+// };
+//
+// function updatePerson (obj) {
+//     // obj = {
+//     //     name: 'Andrew',
+//     //     age: 24
+//     // }
+//     obj.age = 24;
+// }
+//
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+var grades = [10, 15];
+
+function updateGrades (gradesArr) {
+    // input = [10, 15, 20];
+
+    gradesArr.push(20);
+    debugger;
+};
+
+updateGrades(grades);
+
+console.log(grades);

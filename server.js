2d47bbbe084e15e99f88a2bee75a7e4b1b7996d0
Ola Klingberg <ola@olaklingberg.com>
2016-10-20 15:25:58 -0400

    Converts CRUD requests to use current user todos only.

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ app.get('/', function (req, res) {
 
 app.get('/todos', middleware.requireAuthentication, function (req, res) {
     var query = req.query;
-    var where = {};
+    var where = {
+        userId: req.user.get('id')
+    };
 
     if ( query.hasOwnProperty('completed') ) {
         if ( query.completed === 'true' ) {
@@ -59,7 +61,12 @@ app.get('/todos', middleware.requireAuthentication, function (req, res) {
 app.get('/todos/:id', middleware.requireAuthentication, function (req, res) {
     var todoId = parseInt(req.params.id, 10);
 
-    db.todo.findById(todoId)
+    db.todo.findOne({
+        where: {
+            id: todoId,
+            userId: req.user.get('id')
+        }
+    })
         .then(function (todo) {
             if ( !!todo ) {
                 res.json(todo.toJSON());
@@ -93,7 +100,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function (req, res) {
 
     db.todo.destroy({
         where: {
-            id: todoId
+            id: todoId,
+            userId: req.user.get('id')
         }
     }).then(function (rowsDeleted) {
         if ( rowsDeleted === 0 ) {
@@ -122,31 +130,63 @@ app.delete('/todos/:id', middleware.requireAuthentication, function (req, res) {
 });
 
 
+// app.put('/todos/:id', middleware.requireAuthentication, function (req, res) {
+//     var todoId = parseInt(req.params.id, 10);
+//     var matchedTodo = _.findWhere(todos, {id: todoId});
+//     var body = _.pick(req.body, 'description', 'completed');
+//     var validAttributes = {};
+//
+//     if ( !matchedTodo ) {
+//         return res.status(404).send();
+//     }
+//     if ( body.hasOwnProperty('completed') && _.isBoolean(body.completed) ) {
+//         validAttributes.completed = body.completed;
+//     } else if ( body.hasOwnProperty('completed') ) {
+//         return res.status(400).send();
+//     }
+//
+//     if ( body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0 ) {
+//         validAttributes.description = body.description;
+//     } else if ( body.hasOwnProperty('description') ) {
+//         return res.status(400).send();
+//     }
+//     _.extend(matchedTodo, validAttributes);
+//     res.json(matchedTodo);
+// });
+
 app.put('/todos/:id', middleware.requireAuthentication, function (req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
     var body = _.pick(req.body, 'description', 'completed');
-    var validAttributes = {};
+    var attributes = {};
 
-    if ( !matchedTodo ) {
-        return res.status(404).send();
-    }
-    if ( body.hasOwnProperty('completed') && _.isBoolean(body.completed) ) {
-        validAttributes.completed = body.completed;
-    } else if ( body.hasOwnProperty('completed') ) {
-        return res.status(400).send();
+    if ( body.hasOwnProperty('completed') ) {
+        attributes.completed = body.completed;
     }
 
-    if ( body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0 ) {
-        validAttributes.description = body.description;
-    } else if ( body.hasOwnProperty('description') ) {
-        return res.status(400).send();
+    if (body.hasOwnProperty('description')) {
+        attributes.description = body.description;
     }
-    _.extend(matchedTodo, validAttributes);
-    res.json(matchedTodo);
 
-
+    db.todo.findOne({
+        where: {
+            id: todoId,
+            userId: req.user.get('id')
+        }
+    }).then(function (todo) {
+        if (todo) {
+            todo.update(attributes).then(function (todo) {
+                res.json(todo.toJSON());
+            }, function (e) {
+                res.status(400).json(e);
+            });
+        } else {
+            res.status(404).send();
+        }
+    }, function() {
+        res.status(500).send();
+    });
 });
+
 
 app.post('/users', function (req, res) {
     var body = _.pick(req.body, 'email', 'password');
